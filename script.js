50377363b9535b97899d282d4c9dc24c2356b1c9
voon220 <peter220@gmail.com>
2022-05-03 14:13:16 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,73 @@
+//Nabar
+const menu = document.querySelector('.menu');
+const navbar = document.querySelector('.navbar');
+
+menu.addEventListener('click', ()=>{
+    navbar.classList.toggle('change')
+    menu.classList.toggle('change')
+})
+//End of navbar
+
+// Section 2 video
+const video = document.querySelector('.video')
+const btn = document.querySelector('.buttons i')
+const bar = document.querySelector('.video-bar')
+
+const playPause = () => {
+    if(video.paused){
+        video.play();
+        btn.className = 'far fa-pause-circle'
+        video.style.opacity = '.7'
+    } else {
+        video.pause();
+        btn.className = 'far fa-play-circle'
+        video.style.opacity = '.3'
+    }
+    
+}
+
+btn.addEventListener('click', ()=>{
+    playPause()
+})
+
+video.addEventListener('timeupdate', () => {
+    const barWidth = video.currentTime / video.duration
+    bar.style.width = `${barWidth * 100}%`
+    if(video.ended) {
+        btn.className = 'far fa-play-circle'
+        video.style.opacity = '.3'
+    }
+})
+// End of section 2 video
+
+// Section 3 pricing card
+// var swiper = new Swiper(".swiper-container", {
+//     effect: "coverflow",
+//     grabCursor: true,
+//     centeredSlides: true,
+//     slidesPerView: "auto",
+//     coverflowEffect: {
+//       rotate: 70,
+//       stretch: 0,
+//       depth: 100,
+//       modifier: 1,
+//       slideShadows: true,
+//     },
+//   });
+
+var swiper = new Swiper(".swiper-container", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 30,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+  });
+
+// End of section 3 pricing card
+
